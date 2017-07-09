d1b773c83be623fbb5ec0ecf65fb8fd6d23e5274
Reza Baita <echo8co@gmail.com>
2017-07-09 22:35:12 +0800

    Adding database settings
    adding database setting based on tutorials

--- ejc_project/settings.py
+++ ejc_project/settings.py
@@ -118,3 +118,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Database settings
+# reference https://realpython.com/learn/start-django/#django-18
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': 'Spacetravel12%',
+    }
+}
